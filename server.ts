e8cfe042b7cc4a56c5d1142c8140af17faddbae3
silviaoh <silvia.jisu.oh@gmail.com>
2022-11-18 17:27:25 +0900

    Add: Procfile

--- server.ts
+++ server.ts
@@ -1,7 +1,5 @@
 const jsonServer = require('json-server')
-
 const auth = require('json-server-auth')
-// const cors = require('cors')
 
 const app = jsonServer.create()
 const router = jsonServer.router('db.json')
@@ -12,16 +10,6 @@ const rules = auth.rewriter({
   userSetting: 660,
   accounts: 660,
 })
-
-// let corsOption = {
-//   origin:
-//     process.env.NODE_ENV === 'production' ? 'https://pre-onboarding-7th-3-2-10.vercel.app' : 'http://localhost:3000',
-//   credentials: true,
-
-// }
-
-// app.use(cors(corsOption))
-// app.options('*', cors())
 
 app.use(rules)
 app.use(auth)
