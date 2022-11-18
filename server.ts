0c35548f496add1afa32a6ffdd0d3efb88becace
silviaoh <silvia.jisu.oh@gmail.com>
2022-11-18 11:50:14 +0900

    ToSet: 깃 초기설정

---
+++ server.ts
@@ -0,0 +1,28 @@
+const jsonServer = require('json-server')
+
+const auth = require('json-server-auth')
+const cors = require('cors')
+
+const app = jsonServer.create()
+const router = jsonServer.router('db.json')
+
+app.db = router.db
+const rules = auth.rewriter({
+  users: 660,
+  userSetting: 660,
+  accounts: 660,
+})
+
+let corsOption = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+}
+
+app.use(cors(corsOption))
+app.use(rules)
+app.use(auth)
+app.use(router)
+
+app.listen(4000, () => {
+  console.log('JSON Server is running...')
+})
